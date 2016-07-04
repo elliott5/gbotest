176f4d532353fb5ff9cf023cb2213c8a0b344bcd
Elliott Stoneham <elliott.stoneham@gmail.com>
2016-07-04 15:04:36 +0100

    first version

---
+++ example.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/elliott5/gboplay"
+)
+
+func main() {
+	fmt.Println(gboplay.Playtime(42))
+}
---
+++ vendor/github.com/elliott5/gobplay/stub.go
@@ -0,0 +1,4 @@
+//go:binary-only-package
+
+package gboplay
+
